4c0d0651c5392043e1a1ca261c123d6e2f3c1ac1
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:08:23 +0200

    Saved in TypeScript Playground at 2024-10-10T08:08:22.617Z

--- digits/index.ts
+++ digits/index.ts
@@ -2,8 +2,26 @@ function setup() {
   createCanvas(400, 400);
   background("black");
   const num = Math.floor(random(0, 1_000_000));
+
   const lastDigit1 = num % 10;
-  const numWithoutLastDigit = Math.floor(num / 10);
+  let numWithoutLastDigit = Math.floor(num / 10);
+
+  const digit2 = numWithoutLastDigit % 10;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+
+  const digit3 = numWithoutLastDigit % 10;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit/ 10);
+
+  const digit4 = numWithoutLastDigit % 10;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+
+  const digit5 = numWithoutLastDigit % 10;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 10);
+
+  const digit6 = numWithoutLastDigit % 10;
+
+
+
   textAlign(CENTER, CENTER);
   fill("yellow");
   noStroke();
@@ -19,10 +37,12 @@ function setup() {
   rect(250, height / 2, 30, 50);
   rect(300, height / 2, 30, 50);
 
-
-
-fill("yellow");
-textSize(30);
-text(`${lastDigit1}`,65, height / 2 +25 );
-text(`${numWithoutLastDigit}`, 125, height / 2 +25)
+  fill("yellow");
+  textSize(30);
+  text(`${lastDigit1}`, 315, height / 2 + 25);
+  text(`${digit2}`, 265, height / 2 + 25);
+  text(`${digit3}`, 215, height / 2 + 25);
+  text(`${digit4}`, 165, height / 2 + 25);
+  text(`${digit5}`, 115, height / 2 + 25);
+  text(`${digit6}`, 65, height / 2 + 25);
 }
