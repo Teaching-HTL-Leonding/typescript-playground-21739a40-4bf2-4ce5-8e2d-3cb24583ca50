61838dc477392ee4b9a51af0d783d1fd0eaf0a88
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 10:33:04 +0200

    Saved in TypeScript Playground at 2024-10-17T08:33:04.474Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -1,9 +1,10 @@
 let num = 0;
-let digit1 = 0
-let digit2 = 0
-let digit3 = 0
-let digit4 = 0
-let digitclicked = 0
+let digit1 = 0;
+let digit2 = 0;
+let digit3 = 0;
+let digit4 = 0;
+let digitclicked = 0;
+let livebinary = 0;
 
 
 function setup() {
@@ -15,7 +16,7 @@ function setup() {
   textAlign(CENTER, CENTER);
   fill("yellow");
   textSize(40);
-  text(`${num} in binary?`, width / 2, height / 4)
+  text(`${num} in binary?`, width / 2, height / 4);
 
   stroke("yellow");
   noFill();
@@ -28,8 +29,8 @@ function setup() {
   fill("yellow");
   text(`${digit1}`, 65, 140, 50, 85);
   text(`${digit2}`, 135, 140, 50, 85);
-  text(`${digit3}`, 205, 140, 50, 85)
-  text(`${digit4}`, 275, 140, 50, 85)
+  text(`${digit3}`, 205, 140, 50, 85);
+  text(`${digit4}`, 275, 140, 50, 85);
 }
 
 function mouseClicked() {
@@ -47,7 +48,7 @@ function mouseClicked() {
     text(`${digit1}`, 65, 140, 50, 85);
   }
 
-    if ((mouseX < 175) && (mouseX > 135) && (mouseY > 140) && (mouseY < 225)) {
+  if ((mouseX < 175) && (mouseX > 135) && (mouseY > 140) && (mouseY < 225)) {
     digitclicked = 4;
     if (digit2 === 0) {
       digit2 = 1;
@@ -58,9 +59,9 @@ function mouseClicked() {
     rect(135, 140, 50, 85);
     fill("yellow");
     text(`${digit2}`, 135, 140, 50, 85);
-    }
+  }
 
-if ((mouseX < 255) && (mouseX > 205) && (mouseY > 140) && (mouseY < 225)) {
+  if ((mouseX < 255) && (mouseX > 205) && (mouseY > 140) && (mouseY < 225)) {
     digitclicked = 4;
     if (digit3 === 0) {
       digit3 = 1;
@@ -71,9 +72,9 @@ if ((mouseX < 255) && (mouseX > 205) && (mouseY > 140) && (mouseY < 225)) {
     rect(205, 140, 50, 85);
     fill("yellow");
     text(`${digit3}`, 205, 140, 50, 85);
-}
+  }
 
-if ((mouseX < 325) && (mouseX > 275) && (mouseY > 140) && (mouseY < 225)) {
+  if ((mouseX < 325) && (mouseX > 275) && (mouseY > 140) && (mouseY < 225)) {
     digitclicked = 4;
     if (digit4 === 0) {
       digit4 = 1;
@@ -84,5 +85,38 @@ if ((mouseX < 325) && (mouseX > 275) && (mouseY > 140) && (mouseY < 225)) {
     rect(275, 140, 50, 85);
     fill("yellow");
     text(`${digit4}`, 275, 140, 50, 85);
-}
+
+
+    num = digit1 * Math.pow(2, 3) + digit2 * Math.pow(2, 2) + digit3 * Math.pow(2, 1) + digit4 * Math.pow(2, 0);
+
+    livebinary = digit1 * Math.pow(2, 3) + digit2 * Math.pow(2, 2) + digit3 * Math.pow(2, 1) + digit4 * Math.pow(2, 0);
+    if (num === livebinary) {
+      background("black");
+      textSize(25);
+      fill("yellow");
+      text(num + ` in binary?`, width / 2, 50);
+      noStroke();
+      fill("green");
+      text("Correct!", width / 2, 250);
+
+
+      stroke("green");
+      noFill();
+      rect(65, 140, 50, 85);
+      rect(135, 140, 50, 85);
+      rect(205, 140, 50, 85);
+      rect(275, 140, 50, 85);
+
+
+      fill("yellow");
+      text(`${digit1}`, 65, 140, 50, 85);
+      text(`${digit2}`, 135, 140, 50, 85);
+      text(`${digit3}`, 205, 140, 50, 85);
+      text(`${digit4}`, 275, 140, 50, 85);
+
+    }
+
+
   }
+
+}
