ed06cc0920d21756c4101c6821bd11081ac6969b
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 08:52:42 +0200

    Saved in TypeScript Playground at 2024-10-17T06:52:42.044Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -4,14 +4,14 @@ function setup() {
   background("black");
   const num = Math.floor(random(0, 4096));
 
-  const lastDigit1 = num % 2;
-  let numWithoutLastDigit = Math.floor(num / 2);
+  const lastDigit1 = num % 16;
+  let numWithoutLastDigit = Math.floor(num / 16);
 
-  const digit2 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
+  const digit2 = numWithoutLastDigit % 16;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 16);
 
-  const digit3 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit/ 2);
+  const digit3 = numWithoutLastDigit % 16;
+  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 16);
 
 
 
@@ -23,19 +23,81 @@ function setup() {
 
   noFill();
   stroke("yellow");
-  rect(50, height / 2, 30, 50);
-  rect(100, height / 2, 30, 50);
-  rect(150, height / 2, 30, 50);
-  rect(200, height / 2, 30, 50);
-  rect(250, height / 2, 30, 50);
-  rect(300, height / 2, 30, 50);
+  rect(120, height / 2, 40, 75);
+  rect(175, height / 2, 40, 75);
+  rect(230, height / 2, 40, 75);
 
   fill("yellow");
   textSize(30);
-  text(`${lastDigit1}`, 315, height / 2 + 25);
-  text(`${digit2}`, 265, height / 2 + 25);
-  text(`${digit3}`, 215, height / 2 + 25);
-  text(`${digit4}`, 165, height / 2 + 25);
-  text(`${digit5}`, 115, height / 2 + 25);
-  text(`${digit6}`, 65, height / 2 + 25);
+
+  if (lastDigit1 === 10) {
+    text(`A`, 250, height / 2 + 37.5)
+  }
+  else if(lastDigit1 === 11){
+    text(`B`, 250, height / 2 + 37.5)
+  } 
+  else if(lastDigit1 === 12){
+    text(`C`, 250, height / 2 + 37.5)
+  }
+    else if(lastDigit1 === 13){
+    text(`D`, 250, height / 2 + 37.5)
+ }  
+ else if(lastDigit1 === 14){
+    text(`E`, 250, height / 2 + 37.5)
+  }
+    else if(lastDigit1 === 15){
+    text(`F`, 250, height / 2 + 37.5)
+  } 
+  else {
+    text(`${lastDigit1}`,250, height / 2 + 37.5 )
+  }
+ 
+
+
+   if (digit2 === 10) {
+    text(`A`, 195, height / 2 + 37.5)
+  }
+  else if(digit2 === 11){
+    text(`B`, 195, height / 2 + 37.5)
+  } 
+  else if(digit2 === 12){
+    text(`C`, 195, height / 2 + 37.5)
+  }
+    else if(digit2 === 13){
+    text(`D`, 195, height / 2 + 37.5)
+ }  
+ else if(digit2 === 14){
+    text(`E`, 195, height / 2 + 37.5)
+  }
+    else if(digit2 === 15){
+    text(`F`, 195, height / 2 + 37.5)
+  }
+  else {
+    text(`${digit2}`,195, height / 2 + 37.5 )
+  }
+
+    
+    
+    
+   if (digit3 === 10) {
+    text(`A`, 140, height / 2 + 37.5)
+  }
+  else if(digit3 === 11){
+    text(`B`, 140, height / 2 + 37.5)
+  } 
+  else if(digit3 === 12){
+    text(`C`, 140, height / 2 + 37.5)
+  }
+    else if(digit3 === 13){
+    text(`D`, 140, height / 2 + 37.5)
+ }  
+ else if(digit3 === 14){
+    text(`E`, 140, height / 2 + 37.5)
+  }
+    else if(digit3 === 15){
+    text(`F`, 140, height / 2 + 37.5)
+  }
+  else {
+    text(`${digit3}`,140, height / 2 + 37.5 )
+  }
 }
