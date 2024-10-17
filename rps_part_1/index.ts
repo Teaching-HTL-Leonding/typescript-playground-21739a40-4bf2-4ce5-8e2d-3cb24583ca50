197ab9a3d408235ac93943f4443e58f7cbf5e99a
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 10:51:00 +0200

    Saved in TypeScript Playground at 2024-10-17T08:51:00.357Z

---
+++ rps_part_1/index.ts
@@ -0,0 +1,31 @@
+// We declare constants to avoid repeating "magic numbers". 
+// These numbers are used for positioning text and icons on the canvas.
+// Note that we use all-caps for constants that represent 
+// configuration values to make them easy to recognize.
+const TEXT_LEFT = 30;
+const STONE_LEFT = 50;
+const PAPER_LEFT = 175;
+const SCISSORS_LEFT = 300;
+const ICON_WIDTH = 100;
+const ICON_TOP = 75;
+const ICON_HEIGHT = 100;
+
+function setup() {
+    createCanvas(500, 490);
+    background("black");
+
+    fill("yellow");
+    textSize(30);
+    text("Human:", TEXT_LEFT, 50);
+
+    // Display the icons for "stone", "paper", and "scissors".
+    textSize(75);
+    text(`🪨`, STONE_LEFT, 150);
+    text(`📃`, STONE_LEFT + 125, 150);
+    text(`✂️ `,STONE_LEFT +250, 150);
+
+}
+
+function mouseMoved() {
+      if ((mouseX > 50) && (mouseX < 100 ) && (mouseY > 75) && (mouseY < 75+100))
+}
