afa4816703ae186be1a85750d0223d6a1baa0af5
Rainer Stropek <rainer@timecockpit.com>
2024-10-23 09:32:13 +0200

    Saved in TypeScript Playground at 2024-10-23T07:32:12.837Z

--- rps_part_1/index.ts
+++ rps_part_1/index.ts
@@ -5,9 +5,9 @@ const SCISSORS_LEFT = 300;
 const ICON_WIDTH = 100;
 const ICON_TOP = 75;
 const ICON_HEIGHT = 100;
-let userChoice = null;  
-let computerChoice = null;  
-let resultText = "";  
+let userChoice = null;
+let computerChoice = null;
+let resultText = "";
 
 function setup() {
     createCanvas(500, 490);
@@ -21,7 +21,7 @@ function setup() {
     textSize(75);
     text(`🪨`, STONE_LEFT, 150);
     text(`📃`, STONE_LEFT + 125, 150);
-    text(`✂️ `,STONE_LEFT + 250, 150);
+    text(`✂️ `, STONE_LEFT + 250, 150);
 }
 
 function mouseMoved() {
@@ -31,14 +31,14 @@ function mouseMoved() {
     // Reset the stroke and fill before drawing the text
     noStroke();
     fill("yellow");
-    
+
     // Redraw the text and icons since we are clearing the canvas
     textSize(30);
     text("Human:", TEXT_LEFT, 50);
     textSize(75);
     text(`🪨`, STONE_LEFT, 150);
     text(`📃`, STONE_LEFT + 125, 150);
-    text(`✂️ `,STONE_LEFT + 250, 150);
+    text(`✂️ `, STONE_LEFT + 250, 150);
 
     // Check if the mouse is over the "stone" icon area
     if ((mouseX > STONE_LEFT) && (mouseX < STONE_LEFT + ICON_WIDTH) && (mouseY > ICON_TOP) && (mouseY < ICON_TOP + ICON_HEIGHT)) {
@@ -47,20 +47,20 @@ function mouseMoved() {
         stroke("yellow");
         noFill();
         rect(STONE_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
-}
+    }
 
-     if ((mouseX > PAPER_LEFT) && (mouseX < PAPER_LEFT + ICON_WIDTH) && (mouseY > ICON_TOP) && (mouseY < ICON_TOP + ICON_HEIGHT)) {
+    if ((mouseX > PAPER_LEFT) && (mouseX < PAPER_LEFT + ICON_WIDTH) && (mouseY > ICON_TOP) && (mouseY < ICON_TOP + ICON_HEIGHT)) {
         // Draw the rectangle when the mouse is over the stone icon
         strokeWeight(4);
         stroke("yellow");
         noFill();
         rect(PAPER_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
-}
-     if ((mouseX > SCISSORS_LEFT) && (mouseX < SCISSORS_LEFT+ ICON_WIDTH) && (mouseY > ICON_TOP) && (mouseY < ICON_TOP + ICON_HEIGHT)) {
+    }
+    if ((mouseX > SCISSORS_LEFT) && (mouseX < SCISSORS_LEFT + ICON_WIDTH) && (mouseY > ICON_TOP) && (mouseY < ICON_TOP + ICON_HEIGHT)) {
         // Draw the rectangle when the mouse is over the stone icon
         strokeWeight(4);
         stroke("yellow");
         noFill();
         rect(SCISSORS_LEFT, ICON_TOP, ICON_WIDTH, ICON_HEIGHT);
-}
+    }
 }
