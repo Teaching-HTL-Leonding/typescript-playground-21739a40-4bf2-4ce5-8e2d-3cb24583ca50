e3191f6af6f09f040723f14ee08905d5699be6d0
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 14:04:10 +0100

    Saved in TypeScript Playground at 2024-11-06T13:04:10.437Z

--- binary_digits/index.ts
+++ binary_digits/index.ts
@@ -1,49 +1,48 @@
+let num = 0;
+let randomNumber: number;
+let digit1: number = 0;
+let digit2: number = 0;
+let digit3: number = 0;
+let digit4: number = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  randomNumber = Math.floor(random(0, 16));
+
+
+
+  createCanvas(400, 300);
   background("black");
-  const num = Math.floor(random(0, 1_00));
 
-  const lastDigit1 = num % 2;
-  let numWithoutLastDigit = Math.floor(num / 2);
-
-  const digit2 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
-
-  const digit3 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit/ 2);
-
-  const digit4 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
-
-  const digit5 = numWithoutLastDigit % 2;
-  numWithoutLastDigit = Math.floor(numWithoutLastDigit / 2);
-
-  const digit6 = numWithoutLastDigit % 2;
-
-
-
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  text(num, width / 2, height - 20);
-
-
+  background("black");
   noFill();
   stroke("yellow");
-  rect(50, height / 2, 30, 50);
-  rect(100, height / 2, 30, 50);
-  rect(150, height / 2, 30, 50);
-  rect(200, height / 2, 30, 50);
-  rect(250, height / 2, 30, 50);
-  rect(300, height / 2, 30, 50);
-
+  rect(25, height / 2 - 40, 60, 100);
+  rect(125, height / 2 - 40, 60, 100);
+  rect(225, height / 2 - 40, 60, 100);
+  rect(325, height / 2 - 40, 60, 100);
+  noStroke();
   fill("yellow");
-  textSize(30);
-  text(`${lastDigit1}`, 315, height / 2 + 25);
-  text(`${digit2}`, 265, height / 2 + 25);
-  text(`${digit3}`, 215, height / 2 + 25);
-  text(`${digit4}`, 165, height / 2 + 25);
-  text(`${digit5}`, 115, height / 2 + 25);
-  text(`${digit6}`, 65, height / 2 + 25);
+  textSize(50);
+  text(digit1, 40, height / 2 + 25);
+  text(digit2, 145, height / 2 + 25);
+  text(digit3, 245, height / 2 + 25);
+  text(digit4, 345, height / 2 + 25);
+
+  textAlign(CENTER, CENTER);
+  textSize(40);
+  text(`${randomNumber} in Binary?`, width / 2, height / 2 - 115);
+}
+
+
+
+function mouseClicked() {
+
+  if (mouseX >= 25 && mouseX <= 85 && mouseY >= height / 2 - 40 && mouseY <= height / 2 + 60) {
+    if (digit1 === 1) {
+      digit1 = 0;
+    } else {
+      digit1 = 1;
+    }
+
+  }
 }
