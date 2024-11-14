1c05d96610ee88edeb51c91c9499001ead313885
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 08:17:56 +0100

    Saved in TypeScript Playground at 2024-11-14T07:17:55.915Z

--- grid/index.ts
+++ grid/index.ts
@@ -11,7 +11,7 @@ function setup() {
     stroke("lightgray");
 
     // Draw a grid by using a while loop.
-    
+
     // Part 1: Draw vertical lines
     // Here you see how to use a while loop.
     let i = GRID;            // STEP 1: Initialize the loop variable
@@ -19,11 +19,10 @@ function setup() {
         line(i, 0, i, SIZE); // STEP 3: Do whatever you want to do repeatedly
         i += GRID;           // STEP 4: Update the loop variable
     }
- while(i < SIZE){
-    line(0, i, 0, i);
-
-    i += GRID;
- }
+    while (i < SIZE) {
+        line(0, i, 0, i);
+        i += GRID;
+    }
 
     // Part 2: Draw horizontal lines
     // <<< Write your code here
