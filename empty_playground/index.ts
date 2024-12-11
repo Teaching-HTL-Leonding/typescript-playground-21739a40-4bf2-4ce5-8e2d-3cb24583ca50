c15510161a4224d16efedb5751e597b9f975421c
Rainer Stropek <rainer@timecockpit.com>
2024-12-11 10:41:53 +0100

    Saved in TypeScript Playground at 2024-12-11T09:41:52.873Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,15 +1,18 @@
 let length1 = 20;
+let SIZE = 400;
 
 function setup() {
     createCanvas(400, 400);
     background("black");
-    stroke("lime    ");
 
     for(let i = 0; i < width; i += length1 ){
-        line(i  , i, width, i);
-        line(SIZE)
+        stroke("lime    ");
+        line(i  , i, width, i)
+        stroke("yellow");
+        line(SIZE, SIZE, SIZE - i, i)
     }
 }
 
 function draw() {
 }
+
