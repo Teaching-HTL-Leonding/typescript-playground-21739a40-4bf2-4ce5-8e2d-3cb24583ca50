b69d5d13b0eaec384d30534e1b3fde23e13676bc
Rainer Stropek <rainer@timecockpit.com>
2024-12-17 11:40:05 +0100

    Saved in TypeScript Playground at 2024-12-17T10:40:05.185Z

---
+++ word_swirrel/index.ts
@@ -0,0 +1,20 @@
+const WIDTH = 500;
+const HEIGHT = 300;
+
+// Enter the word to guess here.
+//                   vvv
+//                   vvv
+const wordToGuess = "apple";
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
+  background("black");
+
+  // <<< Add your code here
+}
+
+// This method will be called automatically when the user clicks "Guess".
+// The guessed text will be in "textInput".
+function guess(textInput: string) {
+  // <<< Add your code here
+}
