b2251aee605088b16742620ff6f114c054029fc0
Rainer Stropek <rainer@timecockpit.com>
2025-01-09 10:07:27 +0100

    Saved in TypeScript Playground at 2025-01-09T09:07:26.860Z

--- moving_lines_exam/index.ts
+++ moving_lines_exam/index.ts
@@ -4,18 +4,18 @@
 // "number of lines" is the number of lines to draw, can be between 1 and 30
 // "min hue" is the minimum hue (HSB color space), can be between 0 and 360
 // "max hue" is the maximum hue (HSB color space), can be between 0 and 360
-const CONFIGURATION = "5;0-360";
+const CONFIGURATION: string = "5;0-360";
 
-let lineStartX: number = 0;     // Start coordinates of line
-let lineStartY: number = 0;
-let lineEndX: number = 0;       // End coordinates of line
-let lineEndY: number = 0;
-let lineColor: number = 0;
+let lineStartX: number[] = [];     // Start coordinates of line
+let lineStartY: number[] = [];
+let lineEndX: number[] = [];  // End coordinates of line
+let lineEndY: number[] = [];
+let lineColor: number[] = [];
 
-let lineStartDx: number = 0;    // Movement of start point per frame in X and Y direction
-let lineStartDy: number = 0;
-let lineEndDx: number = 0;      // Movement of end point per frame in X and Y direction
-let lineEndDy: number = 0;
+let lineStartDx: number[] = [];    // Movement of start point per frame in X and Y direction
+let lineStartDy: number[] = [];
+let lineEndDx: number[] = [];      // Movement of end point per frame in X and Y direction
+let lineEndDy: number[] = [];
 
 let minColor = 0;               // Lower bound of random hue value
 let maxColor = 360;             // Upper bound of random hue value
@@ -25,50 +25,86 @@ function setup() {
     colorMode(HSB);
 
     // Set random start and end position
-    lineStartX = random(50, 450);
-    lineStartY = random(50, 450);
-    lineEndX = random(50, 450);
-    lineEndY = random(50, 450);
+    for (let i = 0; i < 5; i++) {
+        push();
+        lineStartX: (random(50, 450));
+        lineStartY: (random(50, 450));
+        lineEndX: (random(50, 450));
+        lineEndY: (random(50, 450));
 
-    // Set random movement
-    lineStartDx = random(0, 5);
-    lineStartDy = random(0, 5);
-    lineEndDx = random(0, 5);
-    lineEndDy = random(0, 5);
+        // Set random movement
+        lineStartDx: (random(0, 5));
+        lineStartDy: (random(0, 5));
+        lineEndDx: (random(0, 5));
+        lineEndDy: (random(0, 5));
+    }
 
     // Set random color
-    lineColor = random(minColor, maxColor);
+    lineColor[0] = random(minColor, maxColor);
+
+
 }
 
 function draw() {
     background("black");
+    
+        fill("white");
+    rect(50, 10, 30);
+    rect(10, 10, 30);
+
+    textSize(20);
+    fill("black");
+    text("+", 20, 30);
+    text("-", 60, 30);
+
 
     push();
     // Draw current line
-    stroke(lineColor, 100, 100);
+    stroke(lineColor[0], 100, 100);
     strokeWeight(2);
-    line(lineStartX, lineStartY, lineEndX, lineEndY);
+    line(lineStartX[0], lineStartY[0], lineEndX[0], lineEndY[0]);
 
     // Move start and end point
-    lineStartX += lineStartDx;
-    lineStartY += lineStartDy;
-    lineEndX += lineEndDx;
-    lineEndY += lineEndDy;
+    for (let i = 0; i < 5; i++) {
 
-    // Reverse direction when edge has been reached
-    if (lineStartX < 0 || lineStartX > width) {
-        lineStartDx = -lineStartDx;
-    }
-    if (lineStartY < 0 || lineStartY > height) {
-        lineStartDy = -lineStartDy;
-    }
+  
 
-    if (lineEndX < 0 || lineEndX > width) {
-        lineEndDx = -lineEndDx;
-    }
-    if (lineEndY < 0 || lineEndY > height) {
-        lineEndDy = -lineEndDy;
-    }
 
-    pop();
+        lineStartX[i] += lineStartDx[i];
+        lineStartY[i] += lineStartDy[i];
+        lineEndX[i] += lineEndDx[i];
+        lineEndY[i] += lineEndDy[i];
+
+        // Reverse direction when edge has been reached
+
+        if (lineStartX < [i] || lineStartX > width) {
+            lineStartDx = -lineStartDx;
+        }
+        if (lineStartY < 0 || lineStartY > height) {
+            lineStartDy = -lineStartDy;
+        }
+
+        if (lineEndX < 0 || lineEndX > width) {
+            lineEndDx = -lineEndDx;
+        }
+        if (lineEndY < 0 || lineEndY > height) {
+            lineEndDy = -lineEndDy;
+            pop();
+        }
+    }
+    
+  
+
+
+
+
+
+
+
+
+}
+
+function mouseClicked() {
+
+
 }
