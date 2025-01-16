f8560d826d46cab6ea55948562d4f0137247388d
Rainer Stropek <rainer@timecockpit.com>
2025-01-16 10:21:09 +0100

    Saved in TypeScript Playground at 2025-01-16T09:21:09.292Z

--- parallax_step_1/index.ts
+++ parallax_step_1/index.ts
@@ -2,6 +2,7 @@ let backgroundImages: p5.Image[] = [];
 let backgroundScale:  number = 1;
 let scaledImageWidth: number = 0;
 let scaledImageHeight: number = 0; 
+let scrollPosition: number = 0;
 
 function preload() {
     for (let i = 0; i < winter.length; i++) {
@@ -16,9 +17,22 @@ function setup() {
     scaledImageHeight = backgroundImages[0].height * backgroundScale
     scaledImageWidth = backgroundImages[0].width* backgroundScale
 
+
+
+
 }
 
 function draw() {
+    background("black");
+    
+    
+    if(keyIsDown(39)){
+        scrollPosition -= 5;
+    } else if (keyIsDown(37)){
+       scrollPosition += 5
+    }
+    translate(scrollPosition, 0);
+
     for (let i = 0; i < backgroundImages.length; i++) {
         image(backgroundImages[i], 0, 0, scaledImageWidth, scaledImageHeight);
     }
