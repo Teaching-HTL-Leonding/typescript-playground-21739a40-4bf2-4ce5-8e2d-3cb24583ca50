d0afdb1f50d6c048c8c2db7134f1c0c8c3c8db17
Rainer Stropek <rainer@timecockpit.com>
2025-01-16 09:27:44 +0100

    Saved in TypeScript Playground at 2025-01-16T08:27:43.640Z

---
+++ parallax_step_1/index.ts
@@ -0,0 +1,17 @@
+let backgroundImages: p5.Image[] = [];
+
+function preload() {
+    for (let i = 0; i < winter.length; i++) {
+        backgroundImages.push(loadImage(`${BASE_URL}/winter/${winter[i]}`));
+    }
+}
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    for (let i = 0; i < backgroundImages.length; i++) {
+        image(backgroundImages[i], 0, 0, 500, 500);
+    }
+}
