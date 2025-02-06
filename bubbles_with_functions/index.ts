4b5afa15fbb0951295f19485eef5f58abe9bf462
Rainer Stropek <rainer@timecockpit.com>
2025-02-06 09:45:37 +0100

    Saved in TypeScript Playground at 2025-02-06T08:45:37.515Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -1,47 +1,41 @@
 const circles_x: number[] = [];
 const circles_y: number[] = [];
 const circles_diameter: number[] = [];
- 
+
 let waiting_time = 3000;
 let circle_interval: number;
 let level_interval: number;
- 
+
 let points = 0;
- 
- 
+
 function setup() {
   createCanvas(300, 300);
-  background("black");
-  strokeWeight(2);
-  stroke("lime");
-  noFill();
- 
- addRandomCircle();
-  circle_interval = setInterval(addRandomCircle, 3000);
- 
-}
- 
-function draw() {
-  background("black");
-  if (circles_x.length >= 10) {
-   
-  }
- 
-  for(let i = 0; i < circles_x.length; i ++){
-    circle(circles_x[i], circles_y[i], circles_diameter[i]);
-  }
-}
- 
-function addRandomCircle(){
-  circles_x.push(random(0, width));
-  circles_y.push(random(0, height));
-  circles_diameter.push(random(10, 51));
-}
- 
-function isInside(){
- 
+
+ addRandomCircle()
+ setInterval(addRandomCircle, 3000);
+ noLoop();
 }
 
-function mouseClicked(){
+function draw() {
+  background("black");
+
+  noFill()
+  stroke("lime");
+  strokeWeight(2);
+
+
+  for (let i = 0; i < circles_x.length; i++) {
+    circle(circles_x[i], circles_y[i], circles_diameter[i])
+  }
+}
+
+function addRandomCircle() {
+  circles_x.push(random(width));
+  circles_y.push(random(height));
+  circles_diameter.push(random(10, 51));
+  redraw();
+
+
+
 
 }
