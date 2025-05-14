640d85a0942efc85a1ca9fefa5d84174afabe862
Rainer Stropek <rainer@timecockpit.com>
2025-05-14 08:10:44 +0000

    Saved in TypeScript Playground at 2025-05-14T08:10:44.030Z

---
+++ target_game/index.ts
@@ -0,0 +1,27 @@
+// <<< Add your game variables here
+
+function setup() {
+  createCanvas(800, 600);
+
+  // <<< Add setup logic here
+}
+
+function draw() {
+  background("lightgray");
+
+  // <<< Add drawing logic here
+}
+
+function mousePressed() {
+  // <<< Add mouse pressed logic here
+}
+
+function mouseDragged() {
+  // <<< Add mouse dragged logic here
+}
+
+function mouseReleased() {
+  // <<< Add mouse released logic here
+}
+
+// <<< Add additional functions here
